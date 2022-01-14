46bd408659aafffce2a72fbc405f62d837864155
tk04 <turkisaloufi@gmail.com>
2022-01-13 21:32:48 -0500

    testing heroku deployment 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const app = express();
 require("./db/connect");
 const PORT = 4000 || process.env.PORT;
 const userRoutes = require("./routes/user");
-app.set("trust proxy", true); // specify a single subnet
 app.use(express.json());
 app.use(cookieParser());
 
